86b6eb238b416e5ee62952661505ba8846e0ddc6
ilangermano <ilanpaixaogermano@gmail.com>
2026-03-21 03:10:30 +1300

    Set-up the assignment stuff for when starting it this week

---
+++ AlienThemePark/build/CMakeFiles/AlienThemePark.out.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AlienThemePark.out"
+  "AlienThemePark.out.pdb"
+  "CMakeFiles/AlienThemePark.out.dir/main.cpp.o"
+  "CMakeFiles/AlienThemePark.out.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AlienThemePark.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AlienThemePark/build/CMakeFiles/AlienThemePark.out.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for AlienThemePark.out.
+# This may be replaced when dependencies are built.
