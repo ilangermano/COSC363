2814291ffe8c17c02d4dae03fa8fb4d83f6590d0
Ilan Germano <ige22@uclive.ac.nz>
2026-05-26 15:58:10 +1200

    Finished lab 8

---
+++ Lab08_Files/build/CMakeFiles/RayTracer.out.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/csse/users/ige22/Desktop/COSC363-new/Lab08_Files/Plane.cpp" "CMakeFiles/RayTracer.out.dir/Plane.cpp.o" "gcc" "CMakeFiles/RayTracer.out.dir/Plane.cpp.o.d"
+  "/csse/users/ige22/Desktop/COSC363-new/Lab08_Files/Ray.cpp" "CMakeFiles/RayTracer.out.dir/Ray.cpp.o" "gcc" "CMakeFiles/RayTracer.out.dir/Ray.cpp.o.d"
+  "/csse/users/ige22/Desktop/COSC363-new/Lab08_Files/RayTracer.cpp" "CMakeFiles/RayTracer.out.dir/RayTracer.cpp.o" "gcc" "CMakeFiles/RayTracer.out.dir/RayTracer.cpp.o.d"
+  "/csse/users/ige22/Desktop/COSC363-new/Lab08_Files/SceneObject.cpp" "CMakeFiles/RayTracer.out.dir/SceneObject.cpp.o" "gcc" "CMakeFiles/RayTracer.out.dir/SceneObject.cpp.o.d"
+  "/csse/users/ige22/Desktop/COSC363-new/Lab08_Files/Sphere.cpp" "CMakeFiles/RayTracer.out.dir/Sphere.cpp.o" "gcc" "CMakeFiles/RayTracer.out.dir/Sphere.cpp.o.d"
+  "/csse/users/ige22/Desktop/COSC363-new/Lab08_Files/TextureBMP.cpp" "CMakeFiles/RayTracer.out.dir/TextureBMP.cpp.o" "gcc" "CMakeFiles/RayTracer.out.dir/TextureBMP.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
